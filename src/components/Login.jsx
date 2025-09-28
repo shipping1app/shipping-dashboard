397475b56d64716c459f34a7c9515ef39ee06c92
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-09-28 18:50:10 +0300

    upate Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,7 @@ const Login = () => {
   return (
     <div>
       <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Login Page</button>
+       {/* edited */}
     </div>
   )
 }
